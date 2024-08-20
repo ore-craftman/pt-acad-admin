c904fb5266e9e26009e4851d187541c804ac6723
oreoluwa <ore.craftman@gmail.com>
2024-08-20 11:29:49 +0100

    chore: history data

--- src/components/history/Events.tsx
+++ src/components/history/Events.tsx
@@ -1,5 +1,5 @@
 import { Box, Button, Flex, Image, Text } from "@chakra-ui/react";
-
+import AddIcon from "../../assets/add.svg";
 import GoogleMeetIcon from "../../assets/google-meet.svg";
 import TrashIcon from "../../assets/trash.svg";
 import CalendarIcon from "../../assets/calendar.svg";
@@ -10,7 +10,14 @@ export const EventHistory = () => {
     { title: "Journalism Mastery", isGoogleMeet: true },
   ];
   return (
-    <Box bg="#fff" p={{ base: 3, lg: 5, xl: 6 }} borderRadius="3xl" w="full">
+    <Box
+      bg="#fff"
+      p={{ base: 3, lg: 5, xl: 6 }}
+      minH={340}
+      borderRadius="3xl"
+      w="full"
+      position="relative"
+    >
       <Flex alignItems="center" justifyContent="space-between" mb={6}>
         <Text>Events History</Text>
 
@@ -46,6 +53,27 @@ export const EventHistory = () => {
               </Button>
             </Flex>
           ))}
+        </Flex>
+
+        <Flex
+          justify="center"
+          alignItems="center"
+          mt={6}
+          position="absolute"
+          left={0}
+          bottom={10}
+          w="full"
+        >
+          <Button
+            leftIcon={<Image src={AddIcon} w={6} alt="" />}
+            colorScheme="gray"
+            variant="outline"
+            borderRadius="full"
+            color="gray.600"
+            fontWeight={550}
+          >
+            Create Event
+          </Button>
         </Flex>
       </Box>
     </Box>
